05b87a8002c2b932a0d634b7c9e8c771d3a0fdfe
Anh005 <anhntb0055@gmail.com>
2025-03-26 09:24:35 +0700

    Lab1

---
+++ IS216_Java/ThucHanh/Lab1/BTrenLop_Cau5.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author NTB Anh
+ */
+import java.util.Scanner;
+
+public class BTrenLop_Cau5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        int soDien;
+        try ( // TODO code application logic here
+            Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Nhap so dien (kWh): ");
+            soDien = scanner.nextInt();
+        }
+
+        int giaBanDau = 2000;
+        int giaTren50kw = 2500;
+        int giaTren100kw = 3500;
+        int tongTien;
+        
+        if (soDien <= 0) {
+            soDien = 0;
+        }
+        
+        if (soDien <= 50) {
+            tongTien = soDien * giaBanDau;
+        } else if (soDien <= 100) {
+            tongTien = 50 * giaBanDau + (soDien - 50) * giaTren50kw;
+        } else {
+            tongTien = 50 * giaBanDau+ 50 * giaTren50kw + (soDien - 100) * giaTren100kw;
+        }
+
+        System.out.println("Tong tien dien: " + tongTien + " dong");
+    
+    }
+}
